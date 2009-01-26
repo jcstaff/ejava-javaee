1df5189aebadae1f266241316bad8e5d9a240c25
Jim Stafford <jcstaff@apl.jhu.edu>
2009-01-26 02:03:53 +0000

    merged with jim@670:693 to get Spring 2009 updates
    git-svn-id: https://ejava.info/repos/class/trunk/ejava-javaee@694 6a96efd0-f11b-0410-be78-92d216eb0a58

--- projects/eSport/eLeague/eLeagueBO/src/main/java/ejava/projects/eleague/bo/Venue.java
+++ projects/eSport/eLeague/eLeagueBO/src/main/java/ejava/projects/eleague/bo/Venue.java
@@ -1,17 +1,12 @@
 package ejava.projects.eleague.bo;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
