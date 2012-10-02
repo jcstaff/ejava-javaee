42a7bf5405e78dc1beb8ce5cf20b86fe336db8a7
Jim Stafford <jcstaff@apl.jhu.edu>
2012-10-02 01:07:21 -0400

    ported TransactedQueueSessionTest

--- jmsCore/jmsMechanics/src/test/java/ejava/examples/jmsmechanics/TransactedQueueSessionTest.java
+++ jmsCore/jmsMechanics/src/test/java/ejava/examples/jmsmechanics/TransactedQueueSessionTest.java
@@ -1,18 +1,20 @@
 package ejava.examples.jmsmechanics;
 
+import static org.junit.Assert.*;
+
+
 import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.Message;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
-import javax.naming.InitialContext;
-
-import junit.framework.TestCase;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * This test case performs a test of a transacted session using a queue. 
@@ -21,79 +23,34 @@ import org.apache.commons.logging.LogFactory;
  *
  * @author jcstaff
  */
-public class TransactedQueueSessionTest extends TestCase {
+public class TransactedQueueSessionTest extends JMSTestBase {
     static Log log = LogFactory.getLog(TransactedQueueSessionTest.class);
-    InitialContext jndi;
-    String connFactoryJNDI = System.getProperty("jndi.name.connFactory",
-        "ConnectionFactory");
-    String destinationJNDI = System.getProperty("jndi.name.testQueue",
-        "queue/ejava/examples/jmsMechanics/queue1");
-    String msgCountStr = System.getProperty("multi.message.count", "20");
-    
-    protected ConnectionFactory connFactory;
     protected Destination destination;        
     protected MessageCatcher catcher1;
-    MessageCatcher catcher2;
-    int msgCount;
-    
+    protected MessageCatcher catcher2;
+
+    @Before
     public void setUp() throws Exception {
-        log.debug("getting jndi initial context");
-        jndi = new InitialContext();    
-        log.debug("jndi=" + jndi.getEnvironment());
-
-        assertNotNull("jndi.name.testQueue not supplied", destinationJNDI);
-        new JMSAdminJMX().destroyQueue("queue1")
-                      .deployQueue("queue1", destinationJNDI);
+        destination = (Queue) lookup(queueJNDI);
+        assertNotNull("null destination:" + queueJNDI, destination);
         
-        assertNotNull("jndi.name.connFactory not supplied", connFactoryJNDI);
-        log.debug("connection factory name:" + connFactoryJNDI);
-        connFactory = (ConnectionFactory)jndi.lookup(connFactoryJNDI);
-        
-        log.debug("destination name:" + destinationJNDI);
-        destination = (Queue) jndi.lookup(destinationJNDI);
-        
-        assertNotNull("multi.message.count not supplied", msgCountStr);
-        msgCount = Integer.parseInt(msgCountStr);
-        
-        catcher1 = new MessageCatcher("receiver1");
-        catcher1.setConnFactory(connFactory);
-        catcher1.setDestination(destination);
-
-        catcher2 = new MessageCatcher("receiver2");
-        catcher2.setConnFactory(connFactory);
-        catcher2.setDestination(destination);
-    }
-    
-    protected void tearDown() throws Exception {
-        while (catcher1.isStarted() != true) {
-            log.debug("waiting for catcher1 to start");
-            Thread.sleep(2000);
-        }
-        catcher1.stop();
-        
-        while (catcher2.isStarted() != true) {
-            log.debug("waiting for catcher2 to start");
-            Thread.sleep(2000);
-        }
-        catcher2.stop();
-
-        while (catcher1.isStopped() != true) {
-            log.debug("waiting for catcher1 to stop");
-            Thread.sleep(2000);
-        }
-        while (catcher2.isStopped() != true) {
-            log.debug("waiting for catcher2 to stop");
-            Thread.sleep(2000);
-        }
+        catcher1 = createCatcher("receiver1", destination);
+        catcher2 = createCatcher("receiver2", destination);
     }
 
+    @After
+    public void tearDown() throws Exception {
+    	shutdownCatcher(catcher1);
+    	shutdownCatcher(catcher2);
+    }
+
+    @Test
     public void testTransactedQueueSessionSend() throws Exception {
         log.info("*** testTransactedQueueSessionSend ***");
-        Connection connection = null;
         Session session = null;
         MessageProducer producer = null;
         try {
-            connection = connFactory.createConnection();
+            connection.stop();
             session = connection.createSession(
                     true, Session.AUTO_ACKNOWLEDGE); //<!-- TRUE=transacted
             producer = session.createProducer(destination);
@@ -127,17 +84,16 @@ public class TransactedQueueSessionTest extends TestCase {
         finally {
             if (producer != null) { producer.close(); }
             if (session != null)  { session.close(); }
-            if (connection != null) { connection.close(); }
         }
     }
 
+    @Test
     public void testRollbackTransactedQueueSessionSend() throws Exception {
         log.info("*** testRollbackTransactedQueueSessionSend ***");
-        Connection connection = null;
         Session session = null;
         MessageProducer producer = null;
         try {
-            connection = connFactory.createConnection();
+            connection.stop();
             session = connection.createSession(
                     true, Session.AUTO_ACKNOWLEDGE); //<!-- TRUE=transacted
             producer = session.createProducer(destination);
@@ -167,17 +123,16 @@ public class TransactedQueueSessionTest extends TestCase {
         finally {
             if (producer != null) { producer.close(); }
             if (session != null)  { session.close(); }
-            if (connection != null) { connection.close(); }
         }
     }
-    
+
+    @Test
     public void testTransactedQueueSessionMultiSend() throws Exception {
         log.info("*** testTransactedQueueSessionMultiSend ***");
-        Connection connection = null;
         Session session = null;
         MessageProducer producer = null;
         try {
-            connection = connFactory.createConnection();
+            connection.stop();
             session = connection.createSession(
                     true, Session.AUTO_ACKNOWLEDGE);  //<!-- TRUE=transacted
             producer = session.createProducer(destination);
@@ -208,17 +163,16 @@ public class TransactedQueueSessionTest extends TestCase {
         finally {
             if (producer != null) { producer.close(); }
             if (session != null)  { session.close(); }
-            if (connection != null) { connection.close(); }
         }
     }
 
+    @Test
     public void testRollbackTransactedQueueSessionMultiSend() throws Exception {
         log.info("*** testRollbackTransactedQueueSessionMultiSend ***");
-        Connection connection = null;
         Session session = null;
         MessageProducer producer = null;
         try {
-            connection = connFactory.createConnection();
+            connection.stop();
             session = connection.createSession(
                     true, Session.AUTO_ACKNOWLEDGE);  //<!-- TRUE=transacted
             producer = session.createProducer(destination);
@@ -249,7 +203,6 @@ public class TransactedQueueSessionTest extends TestCase {
         finally {
             if (producer != null) { producer.close(); }
             if (session != null)  { session.close(); }
-            if (connection != null) { connection.close(); }
         }
     }
 }
