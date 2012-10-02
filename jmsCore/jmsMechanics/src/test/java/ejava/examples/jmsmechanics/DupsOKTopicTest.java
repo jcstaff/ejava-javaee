fc179e5af96e2c2be520ae028fba169afc77a81e
Jim Stafford <jcstaff@apl.jhu.edu>
2012-10-01 23:28:26 -0400

    ported DupsOKTopicTest

--- jmsCore/jmsMechanics/src/test/java/ejava/examples/jmsmechanics/DupsOKTopicTest.java
+++ jmsCore/jmsMechanics/src/test/java/ejava/examples/jmsmechanics/DupsOKTopicTest.java
@@ -1,18 +1,18 @@
 package ejava.examples.jmsmechanics;
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
+import static org.junit.Assert.*;
+
 import javax.jms.Destination;
 import javax.jms.Message;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.Topic;
-import javax.naming.InitialContext;
-
-import junit.framework.TestCase;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * This test case performs the basic steps to send/receive messages to/from
@@ -21,86 +21,40 @@ import org.apache.commons.logging.LogFactory;
  *
  * @author jcstaff
  */
-public class DupsOKTopicTest extends TestCase {
+public class DupsOKTopicTest extends JMSTestBase {
     static Log log = LogFactory.getLog(DupsOKTopicTest.class);
-    InitialContext jndi;
-    String connFactoryJNDI = System.getProperty("jndi.name.connFactory",
-        "ConnectionFactory");
-    String destinationJNDI = System.getProperty("jndi.name.testTopic",
-        "topic/ejava/examples/jmsMechanics/topic1");
-    String msgCountStr = System.getProperty("multi.message.count", "20");
-    
-    ConnectionFactory connFactory;
-    Destination destination;        
-    MessageCatcher catcher1;
-    MessageCatcher catcher2;
-    int msgCount;
-    
+    protected Destination destination;        
+    protected MessageCatcher catcher1;
+    protected MessageCatcher catcher2;
+
+    @Before
     public void setUp() throws Exception {
-        log.debug("getting jndi initial context");
-        jndi = new InitialContext();    
-        log.debug("jndi=" + jndi.getEnvironment());
-
-        assertNotNull("jndi.name.testTopic not supplied", destinationJNDI);
-        new JMSAdminJMX().destroyTopic("topic1")
-                      .deployTopic("topic1", destinationJNDI);
-
-        assertNotNull("jndi.name.connFactory not supplied", connFactoryJNDI);
-        log.debug("connection factory name:" + connFactoryJNDI);
-        connFactory = (ConnectionFactory)jndi.lookup(connFactoryJNDI);
+        destination = (Topic) lookup(topicJNDI);
+        assertNotNull("destination null:" + topicJNDI, destination);
         
-        log.debug("destination name:" + destinationJNDI);
-        destination = (Topic) jndi.lookup(destinationJNDI);
-        
-        assertNotNull("multi.message.count not supplied", msgCountStr);
-        msgCount = Integer.parseInt(msgCountStr);
-        
-        catcher1 = new MessageCatcher("subscriber1");
-        catcher1.setConnFactory(connFactory);
-        catcher1.setDestination(destination);
-        catcher1.setAckMode(Session.DUPS_OK_ACKNOWLEDGE);
-        
-        catcher2 = new MessageCatcher("subscriber2");
-        catcher2.setConnFactory(connFactory);
-        catcher2.setDestination(destination);
-        catcher2.setAckMode(Session.DUPS_OK_ACKNOWLEDGE);
+        catcher1 = createCatcher("subscriber1",destination).setAckMode(Session.DUPS_OK_ACKNOWLEDGE);
+        catcher2 = createCatcher("subscriber2",destination).setAckMode(Session.DUPS_OK_ACKNOWLEDGE);
         
         //topics will only deliver messages to subscribers that are 
         //successfully registered prior to the message being published. We
         //need to wait for the catcher to start so it doesn't miss any 
         //messages.
-        new Thread(catcher1).start();
-        new Thread(catcher2).start();
-        while (catcher1.isStarted() != true) {
-            log.debug("waiting for catcher1 to start");
-            Thread.sleep(2000);
-        }
-        while (catcher2.isStarted() != true) {
-            log.debug("waiting for catcher2 to start");
-            Thread.sleep(2000);
-        }
+        startCatcher(catcher1);
+        startCatcher(catcher2);
     }
     
-    protected void tearDown() throws Exception {
-        catcher1.stop();
-        catcher2.stop();
-        while (catcher1.isStopped() != true) {
-            log.debug("waiting for catcher1 to stop");
-            Thread.sleep(2000);
-        }
-        while (catcher2.isStopped() != true) {
-            log.debug("waiting for catcher2 to stop");
-            Thread.sleep(2000);
-        }
+    @After
+    public void tearDown() throws Exception {
+    	shutdownCatcher(catcher1);
+    	shutdownCatcher(catcher2);
     }
 
+    @Test
     public void testTopicSend() throws Exception {
         log.info("*** testTopicSend ***");
-        Connection connection = null;
         Session session = null;
         MessageProducer producer = null;
         try {
-            connection = connFactory.createConnection();
             session = connection.createSession(
                     false, Session.AUTO_ACKNOWLEDGE);
             producer = session.createProducer(destination);
@@ -122,17 +76,15 @@ public class DupsOKTopicTest extends TestCase {
         finally {
             if (producer != null) { producer.close(); }
             if (session != null)  { session.close(); }
-            if (connection != null) { connection.close(); }
         }
     }
-    
+
+    @Test
     public void testTopicMultiSend() throws Exception {
         log.info("*** testTopicMultiSend ***");
-        Connection connection = null;
         Session session = null;
         MessageProducer producer = null;
         try {
-            connection = connFactory.createConnection();
             session = connection.createSession(
                     false, Session.AUTO_ACKNOWLEDGE);
             producer = session.createProducer(destination);
@@ -156,7 +108,6 @@ public class DupsOKTopicTest extends TestCase {
         finally {
             if (producer != null) { producer.close(); }
             if (session != null)  { session.close(); }
-            if (connection != null) { connection.close(); }
         }
     }
 }
