0cda77c6b3c726ca8d0f4c74c904604ba8773ca4
Jim Stafford <jcstaff@apl.jhu.edu>
2012-10-01 23:13:03 -0400

    ported ConnectionTest

--- jmsCore/jmsMechanics/src/test/java/ejava/examples/jmsmechanics/ConnectionTest.java
+++ jmsCore/jmsMechanics/src/test/java/ejava/examples/jmsmechanics/ConnectionTest.java
@@ -1,62 +1,31 @@
 package ejava.examples.jmsmechanics;
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.naming.InitialContext;
-
-import junit.framework.TestCase;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
 
-public class ConnectionTest extends TestCase {
+public class ConnectionTest extends JMSTestBase {
     static Log log = LogFactory.getLog(ConnectionTest.class);
-    InitialContext jndi;
-    String connFactoryJNDI = System.getProperty("jndi.name.connFactory",
-        "ConnectionFactory");
-    String destinationJNDI = System.getProperty("jndi.name.testTopic",
-        "topic/ejava/examples/jmsMechanics/topic1");
-    
-    ConnectionFactory connFactory;
-        
-    public void setUp() throws Exception {
-        log.debug("getting jndi initial context");
-        jndi = new InitialContext();    
-        log.debug("jndi=" + jndi.getEnvironment());
-        
-        assertNotNull("jndi.name.connFactory not supplied", connFactoryJNDI);
-        connFactory = (ConnectionFactory)jndi.lookup(connFactoryJNDI);
-        
-        //assertNotNull("jndi.name.testTopic not supplied", destinationJNDI);
-    }
-    
-    protected void tearDown() throws Exception {
-    }
 
+    @Test
     public void testConnectionMetadata() throws Exception {
         log.info("*** testConnectionMetadata ***");
-        Connection connection=null;
-        try {
-            connection = connFactory.createConnection();
-            log.info("connection.metaData=" + connection.getMetaData());
-            log.info("connection.metaData.JMSMajorVersion=" + 
-                    connection.getMetaData().getJMSMajorVersion());
-            log.info("connection.metaData.JMSMinorVersion=" + 
-                    connection.getMetaData().getJMSMinorVersion());
-            log.info("connection.metaData.JMSProviderName=" + 
-                    connection.getMetaData().getJMSProviderName());
-            log.info("connection.metaData.JMSVersion=" + 
-                    connection.getMetaData().getJMSVersion());
-            log.info("connection.metaData.providerMajorVersion=" + 
-                    connection.getMetaData().getProviderMajorVersion());
-            log.info("connection.metaData.providerMinorVersion=" + 
-                    connection.getMetaData().getProviderMinorVersion());
-            log.info("connection.metaData.providerVersion=" + 
-                    connection.getMetaData().getProviderVersion());
-        }
-        finally {
-            if (connection != null) { connection.close(); }
-        }
+        
+        log.info("connection.metaData=" + connection.getMetaData());
+        log.info("connection.metaData.JMSMajorVersion=" + 
+                connection.getMetaData().getJMSMajorVersion());
+        log.info("connection.metaData.JMSMinorVersion=" + 
+                connection.getMetaData().getJMSMinorVersion());
+        log.info("connection.metaData.JMSProviderName=" + 
+                connection.getMetaData().getJMSProviderName());
+        log.info("connection.metaData.JMSVersion=" + 
+                connection.getMetaData().getJMSVersion());
+        log.info("connection.metaData.providerMajorVersion=" + 
+                connection.getMetaData().getProviderMajorVersion());
+        log.info("connection.metaData.providerMinorVersion=" + 
+                connection.getMetaData().getProviderMinorVersion());
+        log.info("connection.metaData.providerVersion=" + 
+                connection.getMetaData().getProviderVersion());
     }
     
 }
