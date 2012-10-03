eb829b9e8650c9a729db7838e605bb549d1f8de1
Jim Stafford <jcstaff@apl.jhu.edu>
2012-10-03 05:38:20 -0400

    porting jmsNotifier

--- jmsCore/jmsNotifier/src/test/java/ejava/examples/jmsnotifier/SimpleDemoTest.java
+++ jmsCore/jmsNotifier/src/test/java/ejava/examples/jmsnotifier/SimpleDemoTest.java
@@ -7,22 +7,36 @@ import java.util.Properties;
 
 import javax.jms.ConnectionFactory;
 import javax.jms.Topic;
+import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hornetq.jms.server.embedded.EmbeddedJMS;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import ejava.examples.jmsmechanics.JMSAdmin;
+import ejava.examples.jmsmechanics.JMSAdminHornetQ;
 import ejava.examples.jmsmechanics.JMSAdminJMX;
+import ejava.examples.jmsmechanics.JMSTestBase;
 
 public class SimpleDemoTest {
 	static final Log log = LogFactory.getLog(SimpleDemoTest.class);
-	protected static Properties props;
+    protected static boolean jmsEmbedded = Boolean.parseBoolean( 
+		System.getProperty("jms.embedded", "true"));
+    protected static String adminUser = System.getProperty("admin.user", "admin1");
+    protected static String adminPassword = System.getProperty("admin.password", "password");
+    private static EmbeddedJMS server; //used when JMS server embedded in JVM
+    private static Context jndi; //used when JMS server is remote
+
+    protected static Properties props;
 	public static String topicName;
 	public static String topicJNDI;
-	public static String factoryJNDI;
+	public static String connFactoryJNDI;
 
 	/** Creates the topic for use in tests */
 	@BeforeClass
@@ -37,25 +51,66 @@ public class SimpleDemoTest {
         props = new Properties();
         props.load(in);
         //mangle these names to keep from overlapping with statically defined
-        topicName = props.getProperty("jmx.name.testTopic") + "-test";
-		topicJNDI = props.getProperty("jndi.name.testTopic") + "-test";
-		factoryJNDI = props.getProperty("jndi.name.connFactory");
+        topicName = props.getProperty("jmx.name.testTopic");
+		topicJNDI = props.getProperty("jndi.name.testTopic");
+		connFactoryJNDI = props.getProperty("jndi.name.connFactory");
 
-		new JMSAdminJMX().destroyTopic(topicName)
-		              .deployTopic(topicName, topicJNDI);
+		JMSAdmin jmsAdmin=null;
+		ConnectionFactory connFactory=null;
+		if (jmsEmbedded) {
+			log.info("using embedded JMS server");
+			server = new EmbeddedJMS();
+			server.start();
+			
+			connFactory=(ConnectionFactory) server.lookup(connFactoryJNDI);
+	        jmsAdmin=new JMSAdminHornetQ(connFactory, adminUser, adminPassword);
+		}
+		else {
+	        log.debug("getting jndi initial context");
+	        jndi = new InitialContext();    
+	        log.debug("jndi=" + jndi.getEnvironment());
+			
+	        log.debug("connection factory name:" + connFactoryJNDI);
+	        connFactory = (ConnectionFactory)jndi.lookup(connFactoryJNDI);
+	        jmsAdmin=new JMSAdminHornetQ(connFactory, adminUser, adminPassword)
+	        	.setJNDIPrefix("/jboss/exported");
+		}		
+		
+		//jmsAdmin.destroyTopic(topicName)
+		//        .deployTopic(topicName, topicJNDI);
+		jmsAdmin.close();
+	}
+	
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		if (server != null) {
+			server.stop();
+		}
+		if (jndi != null) {
+			jndi.close();
+		}
+	}
+	
+	protected Object lookup(String name) throws NamingException {
+		log.debug("lookup:" + name);
+		return (server != null) ?
+			server.lookup(name) :
+			jndi.lookup(name);	
 	}
 	
 	@Test
 	public void verifyResources() throws Exception {
 		InitialContext jndi = new InitialContext();
-		log.info("looking up JNDI factory:" + factoryJNDI);
+		log.info("looking up JNDI factory:" + connFactoryJNDI);
 		
 		@SuppressWarnings("unused")
-		ConnectionFactory connFactory = (ConnectionFactory)jndi.lookup(factoryJNDI);
+		ConnectionFactory connFactory = (ConnectionFactory)lookup(connFactoryJNDI);
+		assertNotNull("connFactory not found", connFactory);
 		
 		log.info("looking up topic:" + topicJNDI);
 		@SuppressWarnings("unused")
-		Topic topic = (Topic) jndi.lookup(topicJNDI);		
+		Topic topic = (Topic) lookup(topicJNDI);
+		assertNotNull("topic not found", topic);
 	}
 
 	@Test
@@ -66,7 +121,7 @@ public class SimpleDemoTest {
 		String sleep = props.getProperty("publisher.sleep");
 		//String max = props.getProperty("publisher.max");		
 		String pubArgs[] = {
-				"-jndi.name.connFactory", factoryJNDI,
+				"-jndi.name.connFactory", connFactoryJNDI,
 				"-jndi.name.destination", topicJNDI,
 				"-name", name, 
                 "-sleep", sleep,
@@ -77,7 +132,7 @@ public class SimpleDemoTest {
 		String durable0 = props.getProperty("subscriber.durable");
 		String selector0 = props.getProperty("subscriber.selector");
 		final String sub0Args[] = {
-            "-jndi.name.connFactory", factoryJNDI,
+            "-jndi.name.connFactory", connFactoryJNDI,
             "-jndi.name.destination", topicJNDI,
             "-name", name0,
             "-sleep", sleep0,
@@ -96,7 +151,7 @@ public class SimpleDemoTest {
 		String durable1 = props.getProperty("subscriber1.durable");
 		String selector1 = props.getProperty("subscriber1.selector");
 		final String sub1Args[] = {
-            "-jndi.name.connFactory", factoryJNDI,
+            "-jndi.name.connFactory", connFactoryJNDI,
             "-jndi.name.destination", topicJNDI,
             "-name", name1,
             "-sleep", sleep1,
@@ -114,7 +169,7 @@ public class SimpleDemoTest {
 		String durable2 = props.getProperty("subscriber2.durable");
 		String selector2 = props.getProperty("subscriber2.selector");
 		final String sub2Args[] = {
-            "-jndi.name.connFactory", factoryJNDI,
+            "-jndi.name.connFactory", connFactoryJNDI,
             "-jndi.name.destination", topicJNDI,
             "-name", name2,
             "-sleep", sleep2,
@@ -132,7 +187,7 @@ public class SimpleDemoTest {
 		String durable3 = props.getProperty("subscriber3.durable");
 		String selector3 = props.getProperty("subscriber3.selector");
 		final String sub3Args[] = {
-            "-jndi.name.connFactory", factoryJNDI,
+            "-jndi.name.connFactory", connFactoryJNDI,
             "-jndi.name.destination", topicJNDI,
             "-name", name3,
             "-sleep", sleep3,
@@ -150,7 +205,7 @@ public class SimpleDemoTest {
 		String durable4 = props.getProperty("subscriber4.durable");
 		String selector4 = props.getProperty("subscriber4.selector");
 		final String sub4Args[] = {
-            "-jndi.name.connFactory", factoryJNDI,
+            "-jndi.name.connFactory", connFactoryJNDI,
             "-jndi.name.destination", topicJNDI,
             "-name", name4,
             "-sleep", sleep4,
