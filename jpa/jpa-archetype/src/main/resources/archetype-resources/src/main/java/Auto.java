60b9032b73b9574cbe2ce11cd8b1402c28a67510
jcstaff <jcstaff@apl.jhu.edu>
2013-02-09 22:36:20 -0500

    added jpa archetype

---
+++ jpa/jpa-archetype/src/main/resources/archetype-resources/src/main/java/Auto.java
@@ -0,0 +1,69 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import java.io.Serializable;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity @Table(name="EM_AUTO")
+public class Auto implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+    private String make;
+    private String model;
+    private String color;
+    private int mileage;
+        
+    public long getId() {
+        return id;
+    }
+    @SuppressWarnings("unused")
+    private void setId(long id) {
+        this.id = id;
+    }
+    public String getMake() {
+        return make;
+    }
+    public void setMake(String make) {
+        this.make = make;
+    }
+    public int getMileage() {
+        return mileage;
+    }
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder
+		    .append("id=").append(id)
+			.append(", make=").append(make)
+			.append(", model=").append(model)
+			.append(", color=").append(color)
+			.append(", mileage=").append(mileage);
+		return builder.toString();
+	}    
+}
---
+++ jpa/jpa-archetype/src/main/resources/archetype-resources/src/test/java/AutoTest.java
@@ -0,0 +1,102 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.io.ByteArrayInputStream;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class AutoTest {
+    private static Log log = LogFactory.getLog(Auto.class);
+    private static final String PERSISTENCE_UNIT = "${artifactId}-test";
+    private static EntityManagerFactory emf;
+    private EntityManager em;    
+
+    @BeforeClass
+    public static void setUpClass() {
+        log.debug("creating entity manager factory");
+        emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+    }
+    
+    @Before
+    public void setUp() throws Exception {
+        log.debug("creating entity manager");
+        em = emf.createEntityManager();
+        cleanup();
+        em.getTransaction().begin();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        try {
+            log.debug("tearDown() started, em=" + em);
+            if (!em.getTransaction().isActive()) {
+                em.getTransaction().begin();
+                em.getTransaction().commit();            
+            } else if (!em.getTransaction().getRollbackOnly()) {
+                em.getTransaction().commit();                        	
+            } else {
+            	em.getTransaction().rollback();
+            }
+            em.close();
+            log.debug("tearDown() complete, em=" + em);
+        }
+        catch (Exception ex) {
+            log.fatal("tearDown failed", ex);
+            throw ex;
+        }
+     }
+    
+    @AfterClass
+    public static void tesrDownClass() {
+        log.debug("closing entity manager factory");
+        emf.close();
+    }
+    
+    public void cleanup() {
+        em.getTransaction().begin();
+        List<Auto> autos = em.createQuery("select a from Auto a", Auto.class)
+            .getResultList();
+        for (Auto a: autos) {
+            em.remove(a);
+        }
+        em.getTransaction().commit();
+        log.info("removed " + autos.size() + " autos");
+    }
+
+    @Test
+    public void testCreate() {
+        log.info("testCreate");
+        
+        Auto car = new Auto();
+        car.setMake("Chrysler");
+        car.setModel("Gold Duster");
+        car.setColor("Gold");
+        car.setMileage(60*1000);
+        
+        log.info("creating auto:" + car);                        
+        em.persist(car);        
+        
+        assertNotNull("car not found", em.find(Auto.class,car.getId()));
+    }
+}
