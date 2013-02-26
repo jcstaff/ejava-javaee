f6a23c4a2fca59a7ae585ab030e2aa591d30d9c9
jcstaff <jcstaff@apl.jhu.edu>
2013-02-26 01:20:20 -0500

    added @MapsId to relationship materials

--- ormRelations/src/test/java/ejava/examples/orm/rel/ManyToOneUnidirectionalDemo.java
+++ ormRelations/src/test/java/ejava/examples/orm/rel/ManyToOneUnidirectionalDemo.java
@@ -2,10 +2,12 @@ package ejava.examples.orm.rel;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
 
 import ejava.examples.orm.rel.annotated.Media;
 import ejava.examples.orm.rel.annotated.MediaCopy;
+import ejava.examples.orm.rel.annotated.MediaCopy2;
 
 /**
  * 
@@ -21,6 +23,8 @@ public class ManyToOneUnidirectionalDemo extends DemoBase {
     }
 
     /**
+     * This version of MediaCopy uses a READ-ONLY foreign key mapping
+     * based on JPA 1.0
      */
 	@Test
     public void testCreateManyToOne() {
@@ -51,6 +55,49 @@ public class ManyToOneUnidirectionalDemo extends DemoBase {
             ejava.examples.orm.rel.MediaCopyPK pk =
                 new MediaCopyPK(media.getId(), i);
             MediaCopy mc = em.find(MediaCopy.class, pk);
+            log.info("found media copy:" + mc);
+            assertNotNull("media copy not found:" + pk, mc);
+            assertEquals("unexpected mediaId:" + mc.getMediaId(),
+                    mc.getMediaId(), media.getId());
+            assertEquals("unexpected copyNo:" + mc.getCopyNo(), 
+                    mc.getCopyNo(), i);
+            assertNotNull("no media resolved", mc.getMedia());
+        }
+   }    
+
+	/**
+	 * This version of the MediaCopy uses a JPA 2.0 @MapsId annotation to link
+	 * the foreign and primary key properties.
+	 */
+	@Test
+    public void testCreateManyToOneMapsId() {
+        log.info("testCreateManyToOneMapsId");
+        ejava.examples.orm.rel.annotated.Media media = new Media();
+        media.setTitle("EJB31");
+        
+        //add media to DB
+        assertTrue(media.getId() == 0);
+        em.persist(media);
+        log.info("created media:" + media);
+        assertTrue(media.getId() != 0);
+        
+        //create some copies 
+        for(int i=0; i<5; i++) {
+            ejava.examples.orm.rel.annotated.MediaCopy2 mc = 
+                new MediaCopy2(media, i);
+            assertNotNull(mc.getMedia());
+            assertEquals(i, mc.getCopyNo());
+            em.persist(mc);
+            log.info("created copy:" + mc);
+        }
+        
+        em.flush();
+        em.clear();
+        
+        for(int i=0; i<5; i++) {
+            ejava.examples.orm.rel.MediaCopyPK pk =
+                new MediaCopyPK(media.getId(), i);
+            MediaCopy2 mc = em.find(MediaCopy2.class, pk);
             log.info("found media copy:" + mc);
             assertNotNull("media copy not found:" + pk, mc);
             assertEquals("unexpected mediaId:" + mc.getMediaId(),
